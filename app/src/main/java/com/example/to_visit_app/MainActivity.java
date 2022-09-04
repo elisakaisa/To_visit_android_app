7f7beb3c82db7a2e29c2996639660dffec377754
elisakaisa <elisa.k.perini@gmail.com>
2022-09-04 08:39:42 +0200

    bottom navigation done

--- app/src/main/java/com/example/to_visit_app/MainActivity.java
+++ app/src/main/java/com/example/to_visit_app/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.to_visit_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(this::listener);
+
+        //default fragment
+        loadFragment(new FragmentHome());
+        bottomNavigationView.getMenu().getItem(1);
+    }
+
+    private boolean listener(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_add:
+                loadFragment(new FragmentAdd());
+                return true;
+            case R.id.nav_home:
+                loadFragment(new FragmentHome());
+                return true;
+        }
+        return false;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_container, fragment, "");
+        fragmentTransaction.commit();
     }
 }
